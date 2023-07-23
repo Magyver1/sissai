ac006373d218d397c5bad67d80dc3c64d316ba97
Simon <magyver2908@gmail.com>
2023-07-23 15:51:39 -0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const vitrinaArray = [
+    {
+        id: 1,
+        nombre: "Anillo de Oro Cubo Ópalo Andino",
+        categoria: {
+            nombre: "Anillos",
+            id: "anillos"
+        },
+        precio: 3400,
+        imagen: "../../assets/Anillo Cubo Opalo Andino.jpeg"
+    }
+]
